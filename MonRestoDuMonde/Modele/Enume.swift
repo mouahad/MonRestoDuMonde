a17006d228022e5c7f437b193299dd3efc02d564
MYD <myd@macbook-air-de-myd.home>
2018-06-18 15:34:42 +0200

    Class plat

---
+++ MonRestoDuMonde/Modele/Enume.swift
@@ -0,0 +1,18 @@
+//
+//  Enume.swift
+//  MonRestoDuMonde
+//
+//  Created by MYD on 18/06/2018.
+//  Copyright © 2018 MYD. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
