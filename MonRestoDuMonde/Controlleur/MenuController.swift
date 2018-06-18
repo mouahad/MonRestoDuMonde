91c60080d39204cee7d553929798ea2de0abaa7b
MYD <myd@macbook-air-de-myd.home>
2018-06-18 12:48:38 +0200

    ajout des controlleurs principaux

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by MYD on 18/06/2018.
+//  Copyright © 2018 MYD. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
